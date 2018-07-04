e2a7541d8813cdb9a79aaaa776601488b794e9ea
CaymanJava <bosenko_dmitriy@mail.ru>
2018-07-04 23:36:22 +0200

    Add Quick Stick Indicator

---
+++ indicator/src/main/java/pro/crypto/model/request/QSRequest.java
@@ -0,0 +1,22 @@
+package pro.crypto.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import pro.crypto.model.IndicatorType;
+import pro.crypto.model.tick.Tick;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QSRequest {
+
+    private Tick[] originalData;
+
+    private IndicatorType movingAverageType;
+
+    private int period;
+
+}
